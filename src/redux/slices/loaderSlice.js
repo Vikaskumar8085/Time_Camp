2cc0c6f3afba0be2a15b28bdcc41a6ec29b86da1
Vikaskumar8085 <vikaskumar7049002404@gmail.com>
2024-07-09 13:39:39 +0530

    add redux functionality

---
+++ src/redux/slices/loaderSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const LoaderSlice = createSlice({
+    name: "loaderSlice",
+    initialState: {
+        loading: false
+    },
+    reducers: {
+        setLoader: (state, action) => {
+            state.loading = action.payload
+        }
+    }
+})
+export const { setLoader } = LoaderSlice.actions
+export default LoaderSlice.reducer
