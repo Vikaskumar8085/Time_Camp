bdbc538d666b11d549cbc226c66a25febd3b42f5
Vikaskumar8085 <vikaskumar7049002404@gmail.com>
2024-08-05 22:23:37 +0530

    udpate code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,17 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import Wrapper from "../common/wrapper/Wrapper";
-import TabComp from "../common/TabComp";
-import Loader from "../common/Loader";
 
-// import GridListTable from "./GridListTable";
 
 function Home() {
-  // tabstitle
-  const tabstitle = [
-    {
-      title: "one",
-    },
-    {
-      title: "two",
-    },
-    {
-      title: "three",
-    },
-  ];
-  // tabsBody
-  const tabsBody = [
-    {
-      content: "lorem 1",
-    },
-    {
-      content: "lorem 2",
-    },
-
-    {
-      content: "lorem 3",
-    },
-  ];
   return (
     <>
-    <Loader/>
       <Helmet>
         <title> Time Camp</title>
         <link rel="canonical" href="https://www.tacobell.com/" />
       </Helmet>
       <Wrapper>
-        <TabComp Tabsheader={tabstitle} TabsBody={tabsBody} />
+  
       </Wrapper>
     </>
   );
