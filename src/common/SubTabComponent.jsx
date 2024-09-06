49229dd86b3e80b7277df8e3fb3007ecc60811fe
Vikaskumar8085 <vikaskumar7049002404@gmail.com>
2024-09-06 18:59:17 +0530

    Create Tabs for Sub Component

---
+++ src/common/SubTabComponent.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function SubTabComponent({ Tabsheader, TabsBody }) {
+  const [isState, setisState] = useState(0);
+  const handleclick = (prev) => {
+    setisState(prev);
+  };
+
+  const titletabs = Tabsheader.map((item, index) => {
+    return (
+      <section className="Tabs_box">
+        <div
+          className={"Tabs_head"}
+          key={index}
+          onClick={() => handleclick(index)}
+        >
+          <h1 className={`Tabs_title ${isState === index ? "active" : ""}`}>
+            {item.title}
+          </h1>
+        </div>
+      </section>
+    );
+  });
+
+  const tbody = TabsBody?.map((item, index) => {
+    return (
+      <div className="TabsBody">
+        {isState === index ? <div>{item?.content}</div> : ""}
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <div className="Tabs_container">{titletabs}</div>
+      <div className="Tabsbody_wrapper">{tbody}</div>
+    </>
+  );
+}
+
+export default SubTabComponent;
