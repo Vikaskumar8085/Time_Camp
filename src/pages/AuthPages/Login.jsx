f54a30a5c7fa6dc473b448404c99d610fddc710d
Vikaskumar8085 <vikaskumar7049002404@gmail.com>
2024-09-30 18:52:33 +0530

    update code

--- src/pages/AuthPages/Login.jsx
+++ src/pages/AuthPages/Login.jsx
@@ -17,23 +17,38 @@ function Login() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const employeeRef = React.useRef(null);
+  const contractorRef = React.useRef(null);
+
   // handle submit
   const handleSubmit = async (values) => {
-    try {
-      dispatch(setLoader(true));
-      const response = await loginauth(values);
-      if (response?.data?.success) {
-        dispatch(setLoader(false));
-        dispatch(setLogin(response?.data?.data));
-        toast.success(response?.data?.message);
-        window.location.href = "/dashboard";
-      } else {
-        toast.error(response?.data?.message);
+    if (employeeRef === "Employee" || contractorRef === "Contractor") {
+      // e.preventDefault();
+      const selectedTypes = [];
+      if (employeeRef.current.checked) {
+        selectedTypes.push("Employee");
       }
-    } catch (error) {
-      dispatch(setLoader(false));
-      toast.error(error?.response?.data?.message);
-      navigate("/login");
+      if (contractorRef.current.checked) {
+        selectedTypes.push("Contractor");
+      }
+      console.log("Selected Types:", selectedTypes);
+    } else {
+      try {
+        dispatch(setLoader(true));
+        const response = await loginauth(values);
+        if (response?.data?.success) {
+          dispatch(setLoader(false));
+          dispatch(setLogin(response?.data?.data));
+          toast.success(response?.data?.message);
+          window.location.href = "/dashboard";
+        } else {
+          toast.error(response?.data?.message);
+        }
+      } catch (error) {
+        dispatch(setLoader(false));
+        toast.error(error?.response?.data?.message);
+        navigate("/login");
+      }
     }
   };
 
@@ -69,6 +84,18 @@ function Login() {
           <div className="login_box">
             <LoginTitle />
             <LoginForm handleSubmit={handleSubmit} />
+
+            <label>
+              <input type="checkbox" ref={employeeRef} value="employee" />
+              Employee
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" ref={contractorRef} value="contractor" />
+              Contractor
+            </label>
+            <br />
+
             <Button onClick={() => login()} className={"btn-google-auto"}>
               <img src={GoogleIcons} alt="no-image" />
               <span> Login with google </span>
