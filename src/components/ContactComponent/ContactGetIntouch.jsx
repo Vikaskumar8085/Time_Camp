f2d61b7c6eb6565981b8a0cbaeefcb1d9dddf79d
Vikaskumar8085 <vikaskumar7049002404@gmail.com>
2024-10-14 22:22:43 +0530

    make design of contact get in touch

--- src/components/ContactComponent/ContactGetIntouch.jsx
+++ src/components/ContactComponent/ContactGetIntouch.jsx
@@ -1,7 +1,36 @@
-import {Container} from "@mui/system";
+import { Grid, Input } from "@mui/material";
+import { Container } from "@mui/system";
 import React from "react";
+import { useFormik } from "formik";
+import { useDispatch } from "react-redux";
+import GmailIcon from "../../assest/icons/contactIcons/gmail.png";
+import PhoneIcon from "../../assest/icons/contactIcons/phone-call.png";
+import PlaceholderIcon from "../../assest/icons/contactIcons/placeholder.png";
 
 function ContactGetIntouch() {
+  const dispatch = useDispatch();
+  const formik = useFormik({
+    initialValues: {
+      Name: "",
+      Email: "",
+      Subject: "",
+      Dispcription: "",
+    },
+    onSubmit: async (values) => {
+      try {
+        console.log("loadder", values);
+        // const respones = await contactapicall(values);
+        // console.log(respones, "contact");
+        // if (respones.success) {
+        //   console.log("message sent successfully");
+        // }
+        // formik.resetForm();
+      } catch (error) {
+        console.log(error?.message);
+      }
+    },
+  });
+
   return (
     <>
       <section className="contact_get_in_touch">
@@ -11,7 +40,6 @@ function ContactGetIntouch() {
               <div className="contactgetintouch_title">
                 <h1>Get In Touch</h1>
               </div>
-
               <div className="contactgetintouch_paragraph">
                 <p>
                   We would love to hear from you, please fill in the form below
@@ -24,7 +52,110 @@ function ContactGetIntouch() {
 
         <section className="contactgetintouch_section_two">
           <Container>
-            <div className="contactgetintouch_section_two_box"></div>
+            <div className="contactgetintouch_section_two_box">
+              <Grid container spacing={2}>
+                <Grid
+                  className="contact_get_in_touch_section_wrapper"
+                  item
+                  md={6}
+                  sm={6}
+                  lg={6}
+                  xs={12}
+                >
+                  <div className="contactgetintouch_section_left_box">
+                    <div className="contactgetintouch_section_left_box_lists">
+                      <ul>
+                        <li>
+                          <section className="contact-box">
+                            <div className="contact-icons ">
+                              <img
+                                src={GmailIcon}
+                                className={"icons-one"}
+                                alt="no-image"
+                              />
+                            </div>
+                            <div className="contact-content">
+                              <h1>Email Address</h1>
+                              <p>info@ignitivelabs.in</p>
+                            </div>
+                          </section>
+                        </li>
+
+                        <li>
+                          <section className="contact-box">
+                            <div className="contact-icons icons-two">
+                              <img src={PhoneIcon} alt="no-image" />
+                            </div>
+                            <div className="contact-content">
+                              <h1>Mobile No</h1>
+                              <p>918856216422</p>
+                            </div>
+                          </section>
+                        </li>
+                        <li>
+                          <section className="contact-box">
+                            <div className="contact-icons icons-three">
+                              <img src={PlaceholderIcon} alt="no-image" />
+                            </div>
+                            <div className="contact-content">
+                              <h1>Address</h1>
+                              <p>
+                                Ignitive Software Labs, 203, plot No 53, Above
+                                Destiny Cafe, Near Medanta Hospital, Vijaynagar,
+                                Indore (M.P)
+                              </p>
+                            </div>
+                          </section>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </Grid>
+                <Grid item md={6} sm={6} lg={6} xs={12}>
+                  <div className="contactgetintouch_section_right_box">
+                    <form onSubmit={formik.handleSubmit}>
+                      <Grid container spacing={1}>
+                        <Grid item sm={12}>
+                          <Input
+                            labelText={"Name"}
+                            placeholder={"Please enter your Name"}
+                            {...formik.getFieldProps("Name")}
+                          />
+                        </Grid>
+                        <Grid item sm={12}>
+                          <Input
+                            labelText={"Email"}
+                            placeholder={"Please enter your Email Address"}
+                            {...formik.getFieldProps("Email")}
+                          />
+                        </Grid>
+                        <Grid item sm={12}>
+                          <Input
+                            labelText={"Subject"}
+                            placeholder={"Please enter your Subject"}
+                            {...formik.getFieldProps("Subject")}
+                          />
+                        </Grid>
+                        <Grid item sm={12}>
+                          <textarea
+                            cols={2}
+                            placeholder={"Please Enter Your Message"}
+                            rows={10}
+                            id="Dispcription"
+                            name="Dispcription"
+                            {...formik.getFieldProps("Dispcription")}
+                            style={{ width: "100%", margin: "auto" }}
+                          />
+                        </Grid>
+                        <Grid item sm={12}>
+                          <button className={"form_button"}>submit</button>
+                        </Grid>
+                      </Grid>
+                    </form>
+                  </div>
+                </Grid>
+              </Grid>
+            </div>
           </Container>
         </section>
       </section>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+
 import Wrapper from "../common/wrapper/Wrapper";
 import ContactGetIntouch from "../components/ContactComponent/ContactGetIntouch";
 import ContactHead from "../components/ContactComponent/ContactHead";
-import ContactHeader from "../components/ContactComponent/ContactHeader";
 import ContactNewsLetter from "../components/ContactComponent/ContactNewsLetter/ContactNewsLetter";
-import Howcanwehelp from "../components/ContactComponent/Howcanwehelp";
 import LocationMap from "../components/ContactComponent/LocationMap";
 
 function Contact() {
@@ -16,10 +15,8 @@ function Contact() {
       </Helmet>
       <Wrapper>
         <ContactHead />
-        <ContactGetIntouch/>
-        {/* <ContactHeader />
-        <Howcanwehelp /> */}
-        {/* <ContactNewsLetter /> */}
+        <ContactGetIntouch />
+        <ContactNewsLetter />
         <LocationMap />
       </Wrapper>
     </>
